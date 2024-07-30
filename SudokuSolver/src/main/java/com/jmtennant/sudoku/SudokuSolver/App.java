55d5dd55db75053e0e3d02cf7c0a01867bfdd5bb
jmtennan <jmtennan@ncsu.edu>
2024-07-30 17:05:41 -0400

    [Initial Commit] Setup maven project, initial commit to github

---
+++ SudokuSolver/src/main/java/com/jmtennant/sudoku/SudokuSolver/App.java
@@ -0,0 +1,13 @@
+package com.jmtennant.sudoku.SudokuSolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
